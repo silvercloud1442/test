f3925fbe8ea6727b3486a9a2c77425c5204954b7
kudakovazam@yandex.ru <kudakovazam@yandex.ru>
2023-10-17 18:45:09 +0300

    message

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+    $q = $_GET['q'];
+    $params = explode('/', $q);
+    require 'vendor/autoload.php';
+    header('Content-Type: application/json');
+    $client = new MongoDB\Client('mongodb+srv://user:Tunehemah1@cluster0.ghsbmhw.mongodb.net/?retryWrites=true&w=majority');
+
+    $collection = $client->trade->items;
+
+    $result = $collection->find(["base_type" => $params[0]]);
+
+    // if (count($result) === 0)
+    // {
+    //     $res = [
+    //         "status" => false,
+    //         "message" => "Items not found"
+    //     ];
+    //     echo json_encode($res);
+    // }
+
+    foreach ($result as $document){
+        echo json_encode($document);
+    }
+
+
+
+?>
