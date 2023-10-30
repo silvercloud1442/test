2d60ad8b3d960b32415a0045484e27b7b80223eb
Zeliboba <kudakovazam@yandex.ru>
2023-10-30 11:49:08 +0300

    mes

--- index.php
+++ index.php
@@ -4,21 +4,22 @@
     $params = explode('/', $q);
     require 'vendor/autoload.php';
     header('Content-Type: application/json');
-    $client = new MongoDB\Client('mongodb+srv://user:****@cluster0.ghsbmhw.mongodb.net/?retryWrites=true&w=majority');
+    $client = new MongoDB\Client('mongodb+srv://user:Tunehemah1@cluster0.ghsbmhw.mongodb.net/?retryWrites=true&w=majority');
 
     $collection = $client->trade->items;
 
     $result = $collection->find(["base_type" => $params[0]]);
-
-    // if (count($result) === 0)
-    // {
-    //     $res = [
-    //         "status" => false,
-    //         "message" => "Items not found"
-    //     ];
-    //     echo json_encode($res);
-    // }
-
+    $cc = $result;
+    echo count($cc -> toArray());
+    if ($cc === 0)
+    {
+        $res = [
+            "status" => false,
+            "message" => "Items not found"
+        ];
+        echo json_encode($res);
+    }
+    $cc -> close();
     foreach ($result as $document){
         echo json_encode($document);
     }
