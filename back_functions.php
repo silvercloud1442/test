bb409539cae120dabe26f2a54db93d8b04b8b2d1
Zeliboba <kudakovazam@yandex.ru>
2023-11-03 12:05:46 +0300

    aaaaaaaaaaaaaaaaaaaaaa

--- back_functions.php
+++ back_functions.php
@@ -1,7 +1,7 @@
 <?php
 // получиьт данные из коллекции по фильтру
-function get_items($collection, $params){
-    $result = $collection->find($params);
+function get_items($collection, $filter){
+    $result = $collection->find($filter);
     $c = 0;
 
     foreach ($result as $document){
@@ -70,4 +70,100 @@ function params_from_query($query){
         
     }
     return $params;
+}
+
+function pivo($collection){
+    $filter = [
+        '$and' => [
+            [
+                'sockets' => 5
+            ],
+            [
+                'base_type' => 'body_armour'
+            ],
+            [
+                'properties' => [
+                    '$elemMatch' => [
+                        'property' => '+#% to Fire Resistance',
+                        'values' => ['$gte' => 10, '$lte' => 12]
+                    ]
+                ]
+            ],
+            [
+                'properties' => [
+                    '$elemMatch' => [
+                        'property' => '#% increased Stun and Block Recovery',
+                        'values' => ['$gte' => 10]
+                    ]
+                ]
+            ]
+            // Добавьте дополнительные условия для других полей "property", если необходимо
+        ]
+    ];
+
+    $res = $collection->find($filter);
+    foreach ($res as $document){
+        echo json_encode($document);
+    }
+    die();
+}
+
+function pivo2($url){
+/*$url = "http://test?sockets=5&base_type=body_armour&properties=(property1,gte123,lte432;property2,gte111)";*/
+
+    // Разбиваем URL на части, чтобы получить параметры
+    $urlParts = parse_url($url);
+    parse_str($urlParts['query'], $queryParams);
+    
+    // Инициализируем пустой фильтр
+    $filter = ['$and' => []];
+/*
+var_dump($queryParams);
+die();
+// Обрабатываем параметры
+if (isset($queryParams['sockets'])) {
+    $filter['sockets'] = (int)$queryParams['sockets'];
+}
+
+if (isset($queryParams['base_type'])) {
+    $filter['base_type'] = $queryParams['base_type'];
+}*/
+
+    foreach(array_keys($queryParams) as $param){
+        if($param != 'properties'){
+            $value = $queryParams[$param];
+            $converted = floatval($value);
+            if ($converted == $value){
+                $filter[$param] = ['$gte' => $converted];
+            } else {
+                $filter[$param] = $value;
+            }
+        }
+    }
+    
+
+    if (isset($queryParams['properties'])) {
+        $properties = $queryParams['properties'];
+        $parts = explode(';', $properties);
+    
+        foreach ($parts as $part) {
+            // Разбиваем каждую часть на id и значение с помощью разделителя ,
+            list($id, $value) = explode(',', $part);
+            
+            // Создаем элемент фильтра для каждой части
+            $element = [
+                'properties' => [
+                    '$elemMatch' => [
+                        'id' => $id,
+                        'values' => ['$gte' => intval($value)]
+                    ]
+                ]
+            ];
+            
+            // Добавляем элемент в массив $and
+            $filter['$and'][] = $element;
+        }
+    }
+    var_dump($filter);
+    return $filter;
 }
--- index.php
+++ index.php
@@ -8,15 +8,47 @@
     $client = new MongoDB\Client('mongodb+srv://user:Tunehemah1@cluster0.ghsbmhw.mongodb.net/?retryWrites=true&w=majority');
     $collection = $client->trade->items;
 
+   
+
     // получить параметры query из url
     $url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+    // на меня глядит игриво
+    $filter = pivo2($url);
+/*
     $query = query_from_url($url);
     if ($query === false){
         except_invalid_args();
     }
-    // создать словарь параметров
+
+    создать словарь параметров
     $params = params_from_query($query);
 
-    // получиьт данные из коллекции по параметрам
-    get_items($collection, $params);
+    получиьт данные из коллекции по параметрам
+    
+*/
+
+$filter = [
+    '$and' => [
+        [
+            'properties' => [
+                '$elemMatch' => [
+                    'id' => '6522f56cf66388e941146a3e',
+                    'values' => ['$gte' => 1]
+                ]
+            ]
+        ],
+        [
+            'properties' => [
+                '$elemMatch' => [
+                    'id' => '6522f56cf66388e941146a15',
+                    'values' => ['$gte' => 1]
+                ]
+            ]
+        ]
+    ]
+];
+    var_dump($filter);
+
+    get_items($collection, $filter);
 ?>
