ac9e9c654faf68516e0cf76766807a19e0115d7d
Zeliboba <kudakovazam@yandex.ru>
2023-11-21 12:04:09 +0300

    ч

--- properties.php
+++ properties.php
@@ -2,8 +2,10 @@
 function main_properties($db){
         $collection = $db->properties;
         $result = $collection->find([], ['projection' => ['property' => 1]]);
-
+        echo '[';
         foreach ($result as $document){
-            echo json_encode($document);
+            echo  json_encode($document) ;
+            echo ',';
         }
+        echo ']';
     }
