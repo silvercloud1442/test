f05c4ee357e7748ac45d53d2c4a9de88b00c6712
Zeliboba <kudakovazam@yandex.ru>
2023-11-22 09:31:25 +0300

    f

--- items.php
+++ items.php
@@ -44,7 +44,7 @@ function filter_from_url($url){
         }
     }
     if(!isset($queryParams['user'])) {
-        $filter['user'] = ['$not' => $queryParams['user']]
+        $filter['user'] = ['$not' => $queryParams['user']];
     }
     if(!isset($queryParams['price'])) {
         $filter['price'] = ['$exists' => true];
